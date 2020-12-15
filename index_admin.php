8572822eef6577db1ff00deba1a91ef3a72c3188
jc mairot <j.mairot@codeur.online>
2020-12-15 09:44:13 +0100

    read ok

--- index_admin.php
+++ index_admin.php
@@ -6,28 +6,30 @@ require_once('app/database.class.php');
 include('inc/header.php');
 // appel à la BBD
 $pdo=getPdo();
-
+//requête  pour le Read
 $result = $pdo->query("SELECT * FROM gite");
-$gite = $result->fetchall();
+$gites = $result->fetchall();
 
-var_dump($gite);
+// var_dump($gites);
 
 ?>
 
 <!-- formulaire de connexion pour l'accés au dashboard de l'admin -->
-
+<!-- debut foreach -->
+<?php foreach ($gites as $gite): extract($gite) ?> 
 <div class="etiquette">
     <div class="card mb-3" style="max-width: 100%;">
         <div class="row g-0">
+         
             <div class="col-md-3">
-            <div class="imgSizeSquare"></div>
+            <div class="imgSizeSquare" style="background-image:  url(<?=$image_rect_1?>);"></div>
             </div>
                 <div class="col-md-7">
                     <div class="card-body sleep">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p class="card-text"><i class="fas fa-users"> 2 Adultes</i></p>
-                        <h6>Prix</h6>
+                        <h5 class="card-title"><?=$name?></h5>
+                        <p class="card-text"><?=$description?></p>
+                        <p class="card-text"><i class="fas fa-users"> <?=$nbr_couchage?> Adultes</i></p>
+                        <h6><?=$prix?>€</h6>
                     </div>
                 </div>
             <div class="col-md-2">
@@ -40,103 +42,8 @@ var_dump($gite);
         </div>
     </div>
 </div>
-<!-- a effacer -->
-<div class="etiquette">
-    <div class="card mb-3" style="max-width: 100%;">
-        <div class="row g-0">
-            <div class="col-md-3">
-            <div class="imgSizeSquare"></div>
-            </div>
-            <div class="col-md-7">
-            <div class="card-body sleep">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><i class="fas fa-users"> 2 Adultes</i></p>
-                <h6>Prix</h6>
-            </div>
-            </div>
-            <div class="col-md-2">
-            <div class="card-body btnAdmin">
-                <button type="button" class="btn btn-primary">Reserver</button>
-                <button type="button" class="btn btn-primary">Modifier</button>
-                <button type="button" class="btn btn-primary">Supprimer</button>
-            </div>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="etiquette">
-    <div class="card mb-3" style="max-width: 100%;">
-        <div class="row g-0">
-            <div class="col-md-3">
-            <div class="imgSizeSquare"></div>
-            </div>
-            <div class="col-md-7">
-            <div class="card-body sleep">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><i class="fas fa-users"> 2 Adultes</i></p>
-                <h6>Prix</h6>
-            </div>
-            </div>
-            <div class="col-md-2">
-            <div class="card-body btnAdmin">
-                <button type="button" class="btn btn-primary">Reserver</button>
-                <button type="button" class="btn btn-primary">Modifier</button>
-                <button type="button" class="btn btn-primary">Supprimer</button>  
-            </div>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="etiquette">
-    <div class="card mb-3" style="max-width: 100%;">
-        <div class="row g-0">
-            <div class="col-md-3">
-            <div class="imgSizeSquare"></div>
-            </div>
-            <div class="col-md-7">
-            <div class="card-body sleep">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><i class="fas fa-users"> 2 Adultes</i></p>
-                <h6>Prix</h6>
-            </div>
-            </div>
-            <div class="col-md-2">
-            <div class="card-body btnAdmin">
-                <button type="button" class="btn btn-primary">Reserver</button>
-                <button type="button" class="btn btn-primary">Modifier</button>
-                <button type="button" class="btn btn-primary">Supprimer</button> 
-            </div>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="etiquette">
-    <div class="card mb-3" style="max-width: 100%;">
-        <div class="row g-0">
-            <div class="col-md-3">
-            <div class="imgSizeSquare"></div>
-            </div>
-            <div class="col-md-7">
-            <div class="card-body sleep">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><i class="fas fa-users"> 2 Adultes</i></p>
-                <h6>Prix</h6>
-            </div>
-            </div>
-            <div class="col-md-2">
-            <div class="card-body btnAdmin">
-                <button type="button" class="btn btn-primary">Reserver</button>
-                <button type="button" class="btn btn-primary">Modifier</button>
-                <button type="button" class="btn btn-primary">Supprimer</button>
-                
-            </div>
-            </div>
-        </div>
-    </div>
-</div>
+<?php endforeach ?>
+<!-- fin foreach -->
+
 <!-- --------- -->
 <?php include('inc/footer.php');?>
