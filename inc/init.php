7d0e5b54912ae0d2626e37e12682326193ab8cb5
jctest <jcmairot@gmail.com>
2020-12-17 11:55:54 +0100

    mise a jour des fichiers dans dossier class

--- inc/init.php
+++
@@ -1,7 +0,0 @@
-<?php
-// $pdo = new PDO('mysql:host=localhost;dbname=projet_gite', 'root', '', array(
-// 	PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
-// 	PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,	
-// 	PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
-// ));
-
